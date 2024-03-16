52c4f94034ab1fb36b0122c363a026f293899396
Tomer <tomermi123@gmail.com>
2024-03-16 15:55:51 +0200

    implemeting: - clean programming by using global variables and HTML elements references . - fixing size number dot after zero . - initiating the program by updating the used size and progress bar. - adding current file size. - adding box to remove file. - handle file input logic, file type verification and removing it.

--- fylo-solution-main/app.js
+++ fylo-solution-main/app.js
@@ -1,23 +1,75 @@
+const totalSize = 10;
+let usedSize = 0;
+let percentage;
+let sizeLeft;
+const totalSizeElement = document.getElementById('totalSize');
+const sizeLeftElement = document.getElementById('sizeLeft');
+const progressBarElement = document.getElementById('progressBar');
+const usedSizeElement = document.getElementById('sizeUsed');
+const selectedFileElement = document.getElementById('selectedFile');
 
+const shortedSizeNumber = (x) =>{
+    return Number.parseFloat(x).toFixed(2);
+};
 
-function validateFile(fileInput) {
-    const file = fileInput.files[0];
-    if (file) {
-        const fileType = file.type.split('/')[1];
-        if (['jpg', 'jpeg', 'png', 'gif'].includes(fileType)) {
-            // Upload image
-            uploadedFiles.push(file);
-            updateDiskSpace(file.size);
-        } else {
-            alert('File format not supported');
-        }
+const init = () =>{
+  usedSize = Number(window.localStorage.getItem('usedSize'));
+  percentage = 100 * (usedSize / totalSize) +4;
+  sizeLeft = totalSize - usedSize;
+  totalSizeElement.innerText = totalSize + ' MB';
+  sizeLeftElement.innerText = shortedSizeNumber(sizeLeft);
+  usedSizeElement.innerText = shortedSizeNumber(usedSize) + ' MB';
+  progressBarElement.style.width = percentage.toString(10) + '%';
+};
+
+const addCurrentFileSize = (s) => {
+    s /= Math.pow(1024, 2);
+    if (usedSize + s < totalSize) {
+        usedSize += s;
+        sizeLeft = totalSize - usedSize;
+        percentage = 100 * (usedSize / totalSize) - 4;
+        usedSizeElement.innerText = shortedSizeNumber(usedSize) + ' MB';
+        sizeLeftElement.innerText = shortedSizeNumber(sizeLeft);
+        progressBarElement.style.width = percentage.toString(10) + '%';
+        progressBarElement.style.transition = 'width 0.5s ease 0.1s';
+    } else {
+        alert('There is not enough space on the disk');
     }
-}
+};
 
-const uploadImage = document.getElementById('uploadImage');
-const uploadInput = document.getElementById('upload');
+const addFileBox = (name, size) => {
+    const span = document.createElement('span');
+    span.innerText = name;
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'X';
+    removeButton.classList.add('removeButton');
+    removeButton.addEventListener('click', () =>{
+        span.remove();
+        addCurrentFileSize(-size);
+    });
+    span.appendChild(removeButton);
+    selectedFileElement.appendChild(span);
+};
 
-uploadImage.addEventListener('click', () => {
-    uploadInput.click(); // Programmatically trigger click event on file input
-    validateFile(uploadInput);
+
+// Function to handle file input change event
+const handleFileInput = (e) => {
+    const fileName = e.value.split('\\').pop();
+    const isImgFile = new RegExp('(.(gif|jpeg|jpg|png|svg))').test(fileName);
+    if (isImgFile) {
+        const file = e.files[0];
+        addCurrentFileSize(file.size);
+        addFileBox(fileName, file.size);
+    } else {
+        alert('File ' + fileName + ' Type Not Supported');
+    }
+};
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', () => {
+    usedSize = 0;
+    init()
 });
+
+// main start
+init();
